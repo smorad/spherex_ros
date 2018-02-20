a1021645742402d5393bd682b053f85f46085f49
Steven Morad <smorad@email.arizona.edu>
2018-02-20 15:58:20 -0700

    Add marching cubes and poisson reconstruction

--- spherex/spherex_ctrl.py
+++ spherex/spherex_ctrl.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
 from sensor_msgs.msg import PointCloud
+from std_msgs.msg import Float64MultiArray
 
 
 class SpherexCtrl(object):
+    
+    run_once = False
 
     def __init__(self):
         print('Spherex control booting...')
         rospy.init_node('spherex_ctrl')
-        rate = rospy.Rate(1);
+        rate = rospy.Rate(0.5);
         self.input_cloud = rospy.Subscriber(
-                'raw_pointcloud_stream', PointCloud, self.buffer)
-        self.output_cloud = rospy.Publisher('pointcloud_buffer', PointCloud)
+                                            'raw_pointcloud_stream', PointCloud, self.buffer)
+        self.output_cloud = rospy.Publisher(
+                                            'pointcloud_buffer', PointCloud)                
+        self.hop_ctrl = rospy.Publisher(
+                                        'thruster_cmd', Float64MultiArray, queue_size=100)
         while not rospy.is_shutdown():
             rate.sleep()
             self.publish_cloud()
+            if not self.run_once:
+                self.run_once = True
+                self.hop(0, 100, 0, 2, 0)
+                self.hop(100, 0, 0, 2, 0)
+
             
     def buffer(self, data):
         self.cloud = data
+        
+    def hop(self, x, y, z, duration, use_global_frame):
+        hop_impulse = Float64MultiArray(data=[x, y, z, duration, use_global_frame])
+        self.hop_ctrl.publish(hop_impulse)
 
     def publish_cloud(self):
-        self.output_cloud.publish(self.cloud)
-        print('published scan data to processor')
-        #points = point_cloud2.read_points(cloud, skip_nans=True, field_names=("x", "y", "z"))
-        #for p in points:
-        #    self.lidar_stream.publish(points)
+        if hasattr(self, 'cloud') and self.cloud:
+            self.output_cloud.publish(self.cloud)
+            print('published scan data to processor')
             
 
 if __name__ == '__main__':
--- spherex/src/spherex_pcl/main.cpp
+++ spherex/src/spherex_pcl/main.cpp
@@ -20,45 +20,33 @@
 #include <pcl/surface/gp3.h>
 #include <pcl/io/vtk_io.h>
 #include <pcl/io/obj_io.h>
+#include <pcl/io/ply_io.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/surface/poisson.h>
+#include <pcl-1.7/pcl/pcl_base.h>
+#include <pcl-1.7/pcl/surface/reconstruction.h>
+#include <pcl/surface/marching_cubes_rbf.h>
 
-void process_fast_triangulate(const sensor_msgs::PointCloud& input) {
-
-    std::cout << "Processor got msg" << std::endl;
-
-    // Convert from ROS to pcl cloud
+pcl::PointCloud<pcl::PointXYZ>::Ptr build_cloud(
+        const sensor_msgs::PointCloud& input) {
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
-    //pcl::PointCloud<pcl::PointXYZ> cloud = *cloud_ptr;
-
-    std::cout << "pt 1" << std::flush;
-    //pcl::PointCloud<pcl::PointXYZ> cloud = *cloud_ptr;
-    // no library fn to convert from PointCloud, only PointCloud2
-    //cloud.points = input->points;
     cloud_ptr->points.resize(input.points.size());
     for (int i = 0; i < cloud_ptr->points.size(); ++i) {
-
         // Filter NaNs and infs
         if (std::isnan(input.points[i].x) || std::isinf(input.points[i].x)) {
             cloud_ptr->points.erase(cloud_ptr->begin() + i);
             continue;
         }
-        //    continue;
-        //}
-
-        std::cout << "pts" << input.points[i] << std::endl;
         cloud_ptr->points[i].x = input.points[i].x;
         cloud_ptr->points[i].y = input.points[i].y;
         cloud_ptr->points[i].z = input.points[i].z;
-        std::cout << "cloud" << cloud_ptr->points[i] << std::endl;
-
     }
-    std::cout << "cloud size" << cloud_ptr->size() << std::endl;
+    return cloud_ptr;
+}
 
-    std::cout << "pt 2" << std::endl;
-
-
+/* Compute normals and insert them into the cloud*/
+pcl::PointCloud<pcl::PointNormal>::Ptr compute_normals(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr) {
     //Normals
-
     // Create the normal estimation class, and pass the input dataset to it
     pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
     ne.setInputCloud(cloud_ptr);
@@ -71,8 +59,8 @@ void process_fast_triangulate(const sensor_msgs::PointCloud& input) {
     // Output datasets
     pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
 
-    // Use all neighbors in a sphere of radius 3cm
-    ne.setRadiusSearch(0.03);
+    // Use all neighbors in a sphere of radius 10cm
+    ne.setRadiusSearch(0.1);
 
     // Compute the features
     ne.compute(*cloud_normals);
@@ -80,26 +68,56 @@ void process_fast_triangulate(const sensor_msgs::PointCloud& input) {
     // cloud_normals->points.size () should have the same size as the input cloud->points.size ()*
 
 
-    // Normal estimation*
-    /*pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
-    pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
-    tree->setInputCloud(cloud_ptr);
-    n.setInputCloud(cloud_ptr);
-    n.setSearchMethod(tree);
-    n.setKSearch(1); // was 20
-    n.compute(*normals);
-    std::cout << "pt 3" << std::flush;*/
-
 
     // Concatenate the XYZ and normal fields*
     pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals(new pcl::PointCloud<pcl::PointNormal>);
     pcl::concatenateFields(*cloud_ptr, *cloud_normals, *cloud_with_normals);
     //* cloud_with_normals = cloud + normals
+    return cloud_with_normals;
+}
 
-    // Create search tree*
+/* Build a tree of points+normals for faster searching*/
+pcl::search::KdTree<pcl::PointNormal>::Ptr build_tree(pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals) {
     pcl::search::KdTree<pcl::PointNormal>::Ptr tree2(new pcl::search::KdTree<pcl::PointNormal>);
     tree2->setInputCloud(cloud_with_normals);
+    return tree2;
+}
+
+void process_marching_cubes(const sensor_msgs::PointCloud& input) {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr = build_cloud(input);
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals = compute_normals(cloud_ptr);
+    pcl::search::KdTree<pcl::PointNormal>::Ptr tree = build_tree(cloud_with_normals);
+
+    pcl::MarchingCubesRBF<pcl::PointNormal> mc;
+    pcl::PolygonMesh::Ptr triangles(new pcl::PolygonMesh);
+    mc.setInputCloud(cloud_with_normals);
+    mc.setSearchMethod(tree);
+    mc.reconstruct(*triangles);
+    pcl::io::savePLYFile("/home/smorad/marching_cubes.ply", triangles);
+
+}
+
+void process_poisson(const sensor_msgs::PointCloud& input) {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr = build_cloud(input);
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals = compute_normals(cloud_ptr);
+
+    pcl::Poisson<pcl::PointNormal> poisson;
+    pcl::PolygonMesh mesh;
+    poisson.setDepth(7);
+    poisson.setInputCloud(cloud_with_normals);
+    poisson.reconstruct(mesh);
+    pcl::io::savePLYFile("/home/smorad/mesh.ply", mesh);
+}
+
+void process_fast_triangulate(const sensor_msgs::PointCloud& input) {
+
+    //std::cout << "Processor got msg" << std::endl;
+
+    // Convert from ROS to pcl cloud
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr = build_cloud(input);
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals = compute_normals(cloud_ptr);
+    pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 = build_tree(cloud_with_normals);
+
 
     // Initialize objects
     pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
@@ -122,30 +140,6 @@ void process_fast_triangulate(const sensor_msgs::PointCloud& input) {
     gp3.reconstruct(triangles);
     pcl::io::saveOBJFile("/home/smorad/mesh.obj", triangles);
 
-
-
-    /*
-     *     PointCloud<pcl::Normal>::Ptr cloud_normals(new PointCloud<pcl::Normal>());
-
-   pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-   ne.setInputCloud(msg->points);
-   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ> ());
-   ne.setSearchMethod(tree);
-   ne.setRadiusSearch (0.03);
-   ne.compute(*cloud_normals);
-    
-
-   pcl::Poisson<pcl::PointNormal> poisson;
-   poisson.setDepth(9);
-   poisson.setInputCloud(cloud_normals);
-   pcl::PolygonMesh mesh;
-   poisson.reconstruct(mesh);
-     */
-    //  p->performReconstruction()
-    //  BOOST_FOREACH(const pcl::PointXYZ& pt, msg->points) {
-    // compute normal
-    // screened poisson
-    // }
 }
 
 int main(int argc, char** argv) {
@@ -153,6 +147,6 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "pcl_pipeline");
     ros::NodeHandle nh;
     //ros::Subscriber lidar_reader = nh.subscribe("lidar_stream", 1, process_fast_triangulate);
-    ros::Subscriber cloud_reader = nh.subscribe("pointcloud_buffer", 1, process_fast_triangulate);
+    ros::Subscriber cloud_reader = nh.subscribe("pointcloud_buffer", 1, process_poisson);
     ros::spin();
 }
